370adaf384e9705d4909f9aedb2496ad00ef07c5
Cade Loar <cade.loar@gmail.com>
2023-03-07 19:09:52 -0700

    Slight adjustment

--- Mission09_cl464/Infrastructure/SessionExtensions.cs
+++ Mission09_cl464/Infrastructure/SessionExtensions.cs
@@ -7,14 +7,15 @@ using System.Threading.Tasks;
 
 namespace Mission09_cl464.Infrastructure
 {
-    public static class SessionExtension
+    public static class SessionExtensions
     {
         public static void SetJson(this ISession session, string key, object value)
         {
             session.SetString(key, JsonSerializer.Serialize(value));
         }
 
-        public static T GetJson<T> (this ISession session, string key){
+        public static T GetJson<T> (this ISession session, string key)
+        {
             var sessionData = session.GetString(key);
 
             return sessionData == null ? default(T) : JsonSerializer.Deserialize<T>(sessionData);
